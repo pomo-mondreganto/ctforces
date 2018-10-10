dc593c396978fb2bfef041dc2a3d9eea7090270e
pomo-mondreganto <1728390Roman>
2018-10-10 20:06:56 +0300

    Added local settings and config

--- ctforces_backend/ctforces_backend/settings.py
+++ ctforces_backend/ctforces_backend/settings.py
@@ -12,21 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nwyb@ni09jw^gi@r*(u+co1p!u=zm+@k$t9#(9cgw0lt(8v&lo'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-# Application definition
+from ctforces_backend.local_settings import *
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -36,6 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
+    'rest_framework',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
@@ -68,16 +56,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ctforces_backend.wsgi.application'
-
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
