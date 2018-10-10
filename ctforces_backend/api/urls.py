640b91e7f04862304d3b55e0bb270a4d64a50302
pomo-mondreganto <1728390Roman>
2018-10-10 20:09:40 +0300

    Added API schema views

--- ctforces_backend/api/urls.py
+++ ctforces_backend/api/urls.py
@@ -1,7 +1,25 @@
 from django.urls import re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from api import views as api_views
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="CTForces API",
+        default_version='v1',
+        description="Schema of CTForces API. I suggest using schema-redoc/ version due to its beauty :3",
+    ),
+    validators=['flex'],
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     re_path('^$', api_views.test_view, name='test_view')
+
+    re_path(r'^schema_swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^schema_swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^schema_redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
