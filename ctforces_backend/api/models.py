cdb29dcd7dccf57712aa081759b27e654e1c8d9a
pomo-mondreganto <1728390Roman>
2018-10-11 20:46:36 +0300

    Created Post model

--- ctforces_backend/api/models.py
+++ ctforces_backend/api/models.py
@@ -9,3 +9,14 @@ class User(AbstractUser):
 
     created_at = models.DateTimeField(auto_created=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Post(models.Model):
+    author = models.ForeignKey('User', on_delete=models.SET_NULL, related_name='posts', null=True, blank=True)
+    title = models.CharField(max_length=200, blank=False)
+    text = models.TextField(blank=False)
+    is_published = models.BooleanField(default=False)
+    is_main_page = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_created=True)
+    updated_at = models.DateTimeField(auto_now=True)
