178e67dd8d78fa7813234eaca85cef6fbc116127
pomo-mondreganto <1728390Roman>
2018-10-11 20:44:47 +0300

    Added created_at and updated_at for user

--- ctforces_backend/api/models.py
+++ ctforces_backend/api/models.py
@@ -6,3 +6,6 @@ from django.db import models
 class User(AbstractUser):
     username_validator = ASCIIUsernameValidator()
     email = models.EmailField('email address', blank=False, unique=True, null=False)
+
+    created_at = models.DateTimeField(auto_created=True)
+    updated_at = models.DateTimeField(auto_now=True)
