74850db8566e60a2ac656bc7bc314952aa6399ff
Ilya <i.digidi@mail.ru>
2018-10-24 10:55:02 +0300

    Theme update.

--- ctforces_frontend/nuxt.config.js
+++ ctforces_frontend/nuxt.config.js
@@ -3,6 +3,11 @@ const pkg = require('./package');
 export default {
     mode: 'universal',
 
+    server: {
+        port: 8000, // default: 3000
+        host: '0.0.0.0' // default: localhost
+    },
+
     /*
   ** Headers of the page
   */
