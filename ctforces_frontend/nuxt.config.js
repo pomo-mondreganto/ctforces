19451f20e64879e7ef623cd9bf3bf46cdecdea67
Ilya <i.digidi@mail.ru>
2018-10-29 22:18:19 +0300

    Config Update

--- ctforces_frontend/nuxt.config.js
+++ ctforces_frontend/nuxt.config.js
@@ -4,7 +4,7 @@ export default {
     mode: 'universal',
 
     server: {
-        port: 8000, // default: 3000
+        port: 3000, // default: 3000
         host: '0.0.0.0' // default: localhost
     },
 
