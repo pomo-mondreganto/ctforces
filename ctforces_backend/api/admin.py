c925fad9e64cf8a735f8b18944de130bb10f9ebc
pomo-mondreganto <1728390Roman>
2018-11-04 11:57:46 +0300

    Added pretty admin page for posts

--- ctforces_backend/api/admin.py
+++ ctforces_backend/api/admin.py
@@ -191,5 +191,89 @@ class CustomTaskAdmin(admin.ModelAdmin):
     )
 
 
+class CustomPostAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'author',
+        'is_published',
+        'show_on_main_page',
+        'created_at',
+        'updated_at',
+    )
+
+    list_display_links = (
+        'id',
+        'title',
+    )
+
+    fieldsets = (
+        (
+            'Main info',
+            {
+                'fields': (
+                    'title',
+                    'body',
+                    'author',
+                ),
+            }
+        ),
+        (
+            'Publication info',
+            {
+                'fields': (
+                    'is_published',
+                    'show_on_main_page',
+                ),
+            }
+        ),
+        (
+            'Dates',
+            {
+                'fields': (
+                    'created_at',
+                    'updated_at',
+                ),
+            }
+        ),
+    )
+
+    readonly_fields = (
+        'created_at',
+        'updated_at',
+    )
+
+    def get_queryset(self, request):
+        return super(CustomPostAdmin, self).get_queryset(request).select_related('author')
+
+    def publish(self, _request, queryset):
+        queryset.update(is_published=True, publication_time=timezone.now())
+
+    publish.short_description = "Publish selected posts"
+
+    def unpublish(self, _request, queryset):
+        queryset.update(is_published=False)
+
+    unpublish.short_description = "Unpublish selected posts"
+
+    def add_to_main_page(self, _request, queryset):
+        queryset.update(show_on_main_page=True)
+
+    add_to_main_page.short_description = "Add selected posts to main page"
+
+    def remove_from_main_page(self, _request, queryset):
+        queryset.update(show_on_main_page=False)
+
+    remove_from_main_page.short_description = "Remove selected posts from main page"
+
+    actions = (
+        'publish',
+        'unpublish',
+        'add_to_main_page',
+        'remove_from_main_page',
+    )
+
+
 admin.site.register(api_models.User, CustomUserAdmin)
 admin.site.register(api_models.Task, CustomTaskAdmin)
+admin.site.register(api_models.Post, CustomPostAdmin)
