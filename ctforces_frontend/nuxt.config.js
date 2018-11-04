5e24899103c1763e0cc270176394f36a9a3b28ee
pomo-mondreganto <1728390Roman>
2018-11-04 09:13:02 +0300

    Host changed in nuxt config

--- ctforces_frontend/nuxt.config.js
+++ ctforces_frontend/nuxt.config.js
@@ -5,7 +5,7 @@ export default {
 
     server: {
         port: 3000, // default: 3000
-        host: '0.0.0.0' // default: localhost
+      host: '127.0.0.1' // default: localhost
     },
 
     /*
