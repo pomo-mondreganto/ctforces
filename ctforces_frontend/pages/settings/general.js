1d86175680794a4cac43722f58a32ad25f436b4d
pomo-mondreganto <1728390Roman>
2019-01-15 20:55:16 +0300

    Some design fixes

--- ctforces_frontend/pages/settings/general.js
+++ ctforces_frontend/pages/settings/general.js
@@ -1,18 +1,11 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import sidebarLayout from '../../layouts/sidebarLayout';
 import withLayout from '../../wrappers/withLayout';
 import CardWithTabsComponent from '../../components/CardWithTabs';
-import { GlobalCtx } from '../../wrappers/withGlobal';
-import { media_url } from '../../config';
 import FormComponent from '../../components/Form';
 import withAuth from '../../wrappers/withAuth';
 import redirect from '../../lib/redirect';
-
-import { Card, Row, Col } from 'reactstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChartLine, faMarker } from '@fortawesome/free-solid-svg-icons';
 import FileUploaderComponent from '../../components/FileUploaderInput';
-import FileListComponent from '../../components/FileList';
 
 class SettingsSocial extends Component {
     constructor(props) {
@@ -21,7 +14,7 @@ class SettingsSocial extends Component {
 
     onOkSubmit = () => {
         redirect(`user/${this.props.auth.user.username}`);
-        return { ok: true, errors: {} };
+        return {ok: true, errors: {}};
     };
 
     render() {
@@ -33,30 +26,26 @@ class SettingsSocial extends Component {
                             text: this.props.auth.user.username,
                             href: `/user/${this.props.auth.user.username}`
                         },
-                        { text: 'Blog', href: '#' },
-                        { text: 'Tasks', href: '#' },
-                        { text: 'General', href: '/settings/general' },
-                        { text: 'Social', href: '/settings/social' }
+                        {text: 'Blog', href: '#'},
+                        {text: 'Tasks', href: '#'},
+                        {text: 'General', href: '/settings/general'},
+                        {text: 'Social', href: '/settings/social'}
                     ]}
                 >
-                    <Row>
-                        <Col>
-                            <FormComponent
-                                onOkSubmit={this.onOkSubmit}
-                                fields={[
-                                    {
-                                        source: FileUploaderComponent,
-                                        pass_props: {
-                                            upload_url: 'avatar_upload',
-                                            file_upload_name: 'avatar',
-                                            field: 'avatar'
-                                        },
-                                        name: 'avatar'
-                                    }
-                                ]}
-                            />
-                        </Col>
-                    </Row>
+                    <FormComponent
+                        onOkSubmit={this.onOkSubmit}
+                        fields={[
+                            {
+                                source: FileUploaderComponent,
+                                pass_props: {
+                                    upload_url: 'avatar_upload',
+                                    file_upload_name: 'avatar',
+                                    field: 'avatar'
+                                },
+                                name: 'avatar'
+                            }
+                        ]}
+                    />
                 </CardWithTabsComponent>
             </div>
         );
