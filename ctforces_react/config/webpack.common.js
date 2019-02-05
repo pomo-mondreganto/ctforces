635681887ef8c9bd0c115e5f55d9d06d7f6526b1
Ilya <i.digidi@mail.ru>
2019-02-05 20:49:04 +0300

    polyfill added

--- ctforces_react/config/webpack.common.js
+++ ctforces_react/config/webpack.common.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, '../src/index.js'),
+    entry: ['@babel/polyfill', path.resolve(__dirname, '../src/index.js')],
     module: {
         rules: [
             {
