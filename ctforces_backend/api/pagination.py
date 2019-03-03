7e1f96258a25f0a061d3ea218794331724df6f22
pomo-mondreganto <1728390Roman>
2019-03-03 18:39:31 +0300

    Paginators now return page_size

--- ctforces_backend/api/pagination.py
+++ ctforces_backend/api/pagination.py
@@ -1,37 +1,37 @@
-from rest_framework.pagination import PageNumberPagination
+from api.mixins import PageNumberWithPageSizePagination
 
 
-class UserDefaultPagination(PageNumberPagination):
+class UserDefaultPagination(PageNumberWithPageSizePagination):
     page_size = 50
     max_page_size = 100
     page_size_query_param = 'page_size'
 
 
-class UserScoreboardPagination(PageNumberPagination):
+class UserScoreboardPagination(PageNumberWithPageSizePagination):
     page_size = 20
     max_page_size = 20
     page_size_query_param = 'page_size'
 
 
-class TaskDefaultPagination(PageNumberPagination):
+class TaskDefaultPagination(PageNumberWithPageSizePagination):
     page_size = 30
     max_page_size = 50
     page_size_query_param = 'page_size'
 
 
-class TaskFileDefaultPagination(PageNumberPagination):
+class TaskFileDefaultPagination(PageNumberWithPageSizePagination):
     page_size = 30
     max_page_size = 50
     page_size_query_param = 'page_size'
 
 
-class PostDefaultPagination(PageNumberPagination):
+class PostDefaultPagination(PageNumberWithPageSizePagination):
     page_size = 10
     max_page_size = 20
     page_size_query_param = 'page_size'
 
 
-class ContestDefaultPagination(PageNumberPagination):
+class ContestDefaultPagination(PageNumberWithPageSizePagination):
     page_size = 10
     max_page_size = 20
     page_size_query_param = 'page_size'
