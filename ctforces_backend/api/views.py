4a03edb7035a7193fc494c1ff7938c5f17945f27
pomo-mondreganto <1728390Roman>
2019-04-03 23:51:29 +0300

    500 page for testing purposes

--- ctforces_backend/api/views.py
+++ ctforces_backend/api/views.py
@@ -1,7 +1,2 @@
-from django.http import HttpResponse
-from django.views.decorators.http import require_GET
-
-
-@require_GET
 def test_view(request):
-    return HttpResponse('This is a test view')
+    raise Exception("what do you want from me?")
