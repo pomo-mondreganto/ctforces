780410395e5c6003a74267ea4bad82a91f9f5768
xmikasax <xmikasax.main@gmail.com>
2019-04-05 17:11:46 +0300

    hotfix

--- ctforces_react/config/config.js
+++ ctforces_react/config/config.js
@@ -3,7 +3,7 @@ let url = '';
 if (process.env.NODE_ENV === 'development') {
     url = 'http://127.0.0.1:8000';
 } else {
-    url = 'http://ctforces.com';
+    url = 'https://ctforces.com';
 }
 
 const serverUrl = url;
