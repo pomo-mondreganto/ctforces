605b6944dea1e83845f2a80ba31a7c42e501138e
xmikasax <xmikasax.main@gmail.com>
2019-04-05 16:11:38 +0300

    sidebar top users

--- ctforces_react/src/components/Sidebar/Container.js
+++ ctforces_react/src/components/Sidebar/Container.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
 import withAuth from 'wrappers/withAuth';
+import axios from 'axios';
 import Component from './Component';
 
 class SidebarContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    async componentDidMount() {
+        const response = await axios.get('/users/rating_top/');
+        const { data } = response;
+        const { results: users } = data;
+        this.setState({
+            users,
+        });
+    }
+
     render() {
-        return <Component auth={this.props.auth} />;
+        return <Component auth={this.props.auth} topUsers={this.state.users} />;
     }
 }
 
