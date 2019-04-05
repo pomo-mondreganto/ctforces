636e69deb7631204fbe2232052d12673e36dcbee
xmikasax <xmikasax.main@gmail.com>
2019-04-05 20:11:31 +0300

    latex support added

--- ctforces_react/src/components/Markdown/Component.js
+++ ctforces_react/src/components/Markdown/Component.js
@@ -1,8 +1,27 @@
+/* eslint-disable no-shadow */
+/* eslint-disable react/display-name */
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 
-const Component = props => <ReactMarkdown
-    source={props.text}
-/>;
+import RemarkMathPlugin from 'remark-math';
+import 'node_modules/katex/dist/katex.min.css';
+import TeX from '@matejmazur/react-katex';
+
+const Component = (props) => {
+    const newProps = {
+        ...props,
+        plugins: [
+            RemarkMathPlugin,
+        ],
+        renderers: {
+            ...props.renderers,
+            math: props => <TeX math={props.value} block />,
+            inlineMath: props => <TeX math={props.value} />,
+        },
+    };
+    return (
+        <ReactMarkdown {...newProps} source={newProps.text} />
+    );
+};
 
 export default Component;
