d618e97bd46430cff5ed4e2031719ca9e6c3699a
pomo-mondreganto <1728390Roman>
2019-04-17 22:27:11 +0300

    Feedback in contests times resetting

--- ctforces_backend/api/management/commands/reset_contests_times.py
+++ ctforces_backend/api/management/commands/reset_contests_times.py
@@ -13,6 +13,7 @@ class Command(BaseCommand):
         unfinished_contests = unstarted_contests.filter(is_finished=False)
 
         for contest in unstarted_contests:
+            self.stdout.write(f'Processing unstarted contest {contest}')
             celery_app.control.revoke(contest.celery_start_task_id)
             contest.celery_start_task_id = api_tasks.start_contest.apply_async(
                 args=(contest.id,),
@@ -21,6 +22,7 @@ class Command(BaseCommand):
             contest.save()
 
         for contest in unfinished_contests:
+            self.stdout.write(f'Processing unfinished contest {contest}')
             celery_app.control.revoke(contest.celery_end_task_id)
             contest.celery_end_task_id = api_tasks.end_contest.apply_async(
                 args=(contest.id,),
