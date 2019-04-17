89c5076d37737c5095d0bae6e29687d881e7ca93
pomo-mondreganto <1728390Roman>
2019-04-17 21:13:28 +0300

    Custom paginator with fixed page_size

--- ctforces_backend/api/pagination.py
+++ ctforces_backend/api/pagination.py
@@ -1,6 +1,48 @@
+from collections import OrderedDict
+
+import six
+from django.core.paginator import InvalidPage
+from rest_framework.exceptions import NotFound
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
-from api.mixins import PageNumberWithPageSizePagination
+
+class PageNumberWithPageSizePagination(PageNumberPagination):
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('count', self.page.paginator.count),
+            ('page_size', self.page_size),
+            ('next', self.get_next_link()),
+            ('previous', self.get_previous_link()),
+            ('results', data)
+        ]))
+
+    def paginate_queryset(self, queryset, request, view=None):
+        page_size = self.get_page_size(request)
+        if not page_size:
+            return None
+
+        self.page_size = page_size
+
+        paginator = self.django_paginator_class(queryset, page_size)
+        page_number = request.query_params.get(self.page_query_param, 1)
+        if page_number in self.last_page_strings:
+            page_number = paginator.num_pages
+
+        try:
+            self.page = paginator.page(page_number)
+        except InvalidPage as exc:
+            msg = self.invalid_page_message.format(
+                page_number=page_number, message=six.text_type(exc)
+            )
+            raise NotFound(msg)
+
+        if paginator.num_pages > 1 and self.template is not None:
+            # The browsable API should display pagination controls.
+            self.display_page_controls = True
+
+        self.request = request
+        return list(self.page)
 
 
 class UserDefaultPagination(PageNumberWithPageSizePagination):
