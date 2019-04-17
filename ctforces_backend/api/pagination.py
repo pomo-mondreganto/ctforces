09b7ca22009641b1f1593b5c5cc8aa792c61cdd1
pomo-mondreganto <1728390Roman>
2019-04-17 20:57:04 +0300

    Max page size in scoreboard

--- ctforces_backend/api/pagination.py
+++ ctforces_backend/api/pagination.py
@@ -11,7 +11,7 @@ class UserDefaultPagination(PageNumberWithPageSizePagination):
 
 class UserScoreboardPagination(PageNumberWithPageSizePagination):
     page_size = 20
-    max_page_size = 20
+    max_page_size = 100
     page_size_query_param = 'page_size'
 
 
