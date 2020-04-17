be529b76ff0e01e1081d8869e472c91200b4e3e3
Roman Nikitin <nikrom2012@me.com>
2020-04-17 16:19:48 +0300

    Contest tasks caching

--- ctforces_backend/api/contests/caching.py
+++ ctforces_backend/api/contests/caching.py
@@ -1,10 +1,15 @@
 from rest_framework_extensions.key_constructor.bits import (
     ArgsKeyBit,
     KwargsKeyBit,
+    UserKeyBit,
 )
 from rest_framework_extensions.key_constructor.constructors import DefaultKeyConstructor
 
 
-class CTFTimeScoreboardKeyConstructor(DefaultKeyConstructor):
+class ScoreboardKeyConstructor(DefaultKeyConstructor):
     args_bit = ArgsKeyBit()
-    kwargs_bit = KwargsKeyBit
+    kwargs_bit = KwargsKeyBit()
+
+
+class ContestTaskListKeyConstructor(DefaultKeyConstructor):
+    user_bit = UserKeyBit()
