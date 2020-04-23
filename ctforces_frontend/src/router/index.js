5d22347aaab4cac84dfe28164b5102ea235770ab
Ilya <xmikasax.main@gmail.com>
2020-04-23 15:20:36 +0300

    Fix scroll behaviour in router

--- ctforces_frontend/src/router/index.js
+++ ctforces_frontend/src/router/index.js
@@ -11,6 +11,9 @@ const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
     routes,
+    scrollBehavior() {
+        return { x: 0, y: 0 };
+    },
 });
 
 router.beforeEach(async (to, from, next) => {
