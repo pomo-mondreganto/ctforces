39adb98f3150b474f54626afa9eb65ce4cf4cf3c
Ilya <xmikasax.main@gmail.com>
2020-04-23 16:41:48 +0300

    Fix contest tasks scroll

--- ctforces_frontend/src/router/index.js
+++ ctforces_frontend/src/router/index.js
@@ -11,9 +11,6 @@ const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
     routes,
-    scrollBehavior() {
-        return { x: 0, y: 0 };
-    },
 });
 
 router.beforeEach(async (to, from, next) => {
