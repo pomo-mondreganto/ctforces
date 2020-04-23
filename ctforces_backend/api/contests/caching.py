ba80800ba201c2811770e077df8f9bd9b7bec053
Roman Nikitin <nikrom2012@me.com>
2020-04-23 15:13:46 +0300

    Fixed contest task list cache

--- ctforces_backend/api/contests/caching.py
+++ ctforces_backend/api/contests/caching.py
@@ -1,6 +1,7 @@
 from rest_framework_extensions.key_constructor.bits import (
     QueryParamsKeyBit,
     KwargsKeyBit,
+    ArgsKeyBit,
     UserKeyBit,
 )
 from rest_framework_extensions.key_constructor.constructors import DefaultKeyConstructor
@@ -8,8 +9,11 @@ from rest_framework_extensions.key_constructor.constructors import DefaultKeyCon
 
 class ScoreboardKeyConstructor(DefaultKeyConstructor):
     params_bit = QueryParamsKeyBit()
+    args_bit = ArgsKeyBit()
+    kwargs_bit = KwargsKeyBit()
 
 
 class ContestTaskListKeyConstructor(DefaultKeyConstructor):
     user_bit = UserKeyBit()
+    args_bit = ArgsKeyBit()
     kwargs_bit = KwargsKeyBit()
