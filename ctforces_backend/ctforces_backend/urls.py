12fa20e239c866b8b47ce0f4a4c02ed7876f3ed8
Roman Nikitin <nikrom2012@me.com>
2020-04-24 22:56:48 +0300

    Added django-silk

--- ctforces_backend/ctforces_backend/urls.py
+++ ctforces_backend/ctforces_backend/urls.py
@@ -13,14 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, re_path, include
-from django.conf import settings
 from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('silk/', include('silk.urls', namespace='silk')),
 
     re_path('^media/(?P<path>.*)$', serve, {
         'document_root': settings.MEDIA_ROOT,
