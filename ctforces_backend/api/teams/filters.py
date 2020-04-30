60efc5ffb7fb1ad88d33436db350772561d2a062
Roman Nikitin <nikrom2012@me.com>
2020-04-30 14:12:24 +0300

    Rating teams ordering

--- ctforces_backend/api/teams/filters.py
+++ ctforces_backend/api/teams/filters.py
@@ -5,6 +5,11 @@ import api.models
 
 class TeamFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr='istartswith')
+    ordering = django_filters.OrderingFilter(
+        fields=(
+            ('rating', 'rating'),
+        ),
+    )
 
     class Meta:
         model = api.models.Team
